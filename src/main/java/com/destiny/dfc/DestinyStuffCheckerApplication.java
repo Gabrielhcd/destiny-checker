2aa3d162d552f997bcf9632180e3d695534b4a77
Gabriel <gabrielhcarrascod@gmail.com>
2025-08-11 12:34:02 -0600

    initial commit, base project

---
+++ src/main/java/com/destiny/dfc/DestinyStuffCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.destiny.dfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinyStuffCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestinyStuffCheckerApplication.class, args);
+	}
+
+}
